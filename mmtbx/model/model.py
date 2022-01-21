a15e2f24a5d6ffb18a316340cd2adc212e3934e5
Russ Taylor <russ@reliasolve.com>
2022-01-21 12:04:34 -0500

    More descriptive error messages.

--- mmtbx/model/model.py
+++ mmtbx/model/model.py
@@ -2082,7 +2082,7 @@ class manager(object):
       cctbx_depreciation_warning('get_specific_h_bond_type takes int argument')
       i_seq = i_seq.i_seq
     if i_seq>=len(self._type_h_bonds):
-      print('i_seq is larger than array length. Likely due to a unsupported selection.')
+      print('get_specific_h_bond_type(): i_seq is larger than array length. Perhaps due to an unsupported selection.')
     type_h_bond = self._type_h_bonds[i_seq]
     return type_h_bond
 
@@ -2091,7 +2091,7 @@ class manager(object):
       cctbx_depreciation_warning('get_specific_vdw_radii takes int argument')
       i_seq = i_seq.i_seq
     if i_seq>=len(self._type_energies):
-      print('i_seq is larger than array length. Likely due to a unsupported selection.')
+      print('get_specific_vdw_radius(): i_seq is larger than array length. Perhaps due to an unsupported selection.')
     e = self.get_ener_lib()
     type_energy = self._type_energies[i_seq]
     if vdw_radius_without_H:
@@ -2113,7 +2113,7 @@ class manager(object):
       cctbx_depreciation_warning('get_specific_ion_radius takes int argument')
       i_seq = i_seq.i_seq
     if i_seq>=len(self._type_energies):
-      print('i_seq is larger than array length. Likely due to a unsupported selection.')
+      print('get_specific_ion_radius(): i_seq is larger than array length. Perhaps due to an unsupported selection.')
     e = self.get_ener_lib()
     type_energy = self._type_energies[i_seq]
     ion_radii = e.lib_atom[type_energy].ion_radius
